ebb1b98c96a16e14afe4bd9cf8dfc4eb3fe24259
sysy0311 <yusuyeon443@gmail.com>
2024-07-16 15:35:38 +0900

    implement main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용
    테스트는 실행결과만 관측하고 있음, 검증 코드는 없음.

--- fastcampus-java-oop/src/main/java/com/fastcampus/Main.java
+++ fastcampus-java-oop/src/main/java/com/fastcampus/Main.java
@@ -1,17 +1,17 @@
 package com.fastcampus;
 
+import com.fastcampus.logic.BubbleSort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BubbleSort<String> sort = new BubbleSort<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        System.out.println(sort.sort(Arrays.asList(args)));
     }
 }
---
+++ fastcampus-java-oop/src/test/java/com/fastcampus/MainTest.java
@@ -0,0 +1,15 @@
+package com.fastcampus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        String[] args = {"3","1","2"};
+
+        Main.main(args);
+    }
+}
